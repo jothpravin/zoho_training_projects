262332d0a6e1d98de2294cbb0d41449f0bbf33fd
jothpravin <jothipravin5@gmail.com>
2024-02-06 09:07:42 +0530

    add try block

--- Project_StockTrading/Project_StockTrading/src/com/stocktradingsystem/Main.java
+++ Project_StockTrading/Project_StockTrading/src/com/stocktradingsystem/Main.java
@@ -1,5 +1,6 @@
 package com.stocktradingsystem;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main 
@@ -22,93 +23,101 @@ public class Main
         StockManager stockManager = new StockManager(tradingDAO.getStockCount());
         stockManager.setDaemon(true);
         stockManager.start();
-        while(true)
-		{
-			System.out.println(ANSI_BULE+"------------------------------------------------------");
-			System.out.println("| Option |            Description                    |");
-			System.out.println("------------------------------------------------------");
-			System.out.println("|   1    | Login User                                |");
-			System.out.println("|   2    | SignUp User                               |");
-			System.out.println("|   3    | Exit                                      |");
-			System.out.println("------------------------------------------------------"+ANSI_RESET);
+        try
+        {
+            while(true)
+            {
+                System.out.println(ANSI_BULE+"------------------------------------------------------");
+                System.out.println("| Option |            Description                    |");
+                System.out.println("------------------------------------------------------");
+                System.out.println("|   1    | Login User                                |");
+                System.out.println("|   2    | SignUp User                               |");
+                System.out.println("|   3    | Exit                                      |");
+                System.out.println("------------------------------------------------------"+ANSI_RESET);
 
-			System.out.print(ANSI_YELLOW+"Enter the Option to perform :"+ANSI_RESET);	
-			int n = in.nextInt();
-			switch(n)
-			{
-				case 1:
-                    int login = userController.login();
-                    boolean logout = false;
-                    if(login > 0)
-                    {
-                        System.out.println(ANSI_PURUPLE+"Login Successfully as "+userController.getUserName(login)+ANSI_RESET);
-                        while(!logout)
+                System.out.print(ANSI_YELLOW+"Enter the Option to perform :"+ANSI_RESET);	
+                int n = in.nextInt();
+                switch(n)
+                {
+                    case 1:
+                        int login = userController.login();
+                        boolean logout = false;
+                        if(login > 0)
                         {
-                            System.out.println(ANSI_BULE+"------------------------------------------------------");
-                            System.out.println("| Option |            Description                    |");
-                            System.out.println("------------------------------------------------------");
-                            System.out.println("|   1    | Buy Stocks                                |");
-                            System.out.println("|   2    | Sell Stocks                               |");
-                            System.out.println("|   3    | Portfolio                                 |");
-                            System.out.println("|   4    | Create Trading Account                    |");
-                            System.out.println("|   5    | Profile                                   |");
-                            System.out.println("|   6    | Logout                                    |");
-                            System.out.println("------------------------------------------------------"+ANSI_RESET);
-
-                            System.out.print(ANSI_YELLOW+"Enter the Option to perform :"+ANSI_RESET);	
-                            int m = in.nextInt();
-                            switch(m)
+                            System.out.println(ANSI_PURUPLE+"Login Successfully as "+userController.getUserName(login)+ANSI_RESET);
+                            while(!logout)
                             {
-                                case 1:
-                                    tradingController.buyStock();
-                                    break;
+                                System.out.println(ANSI_BULE+"------------------------------------------------------");
+                                System.out.println("| Option |            Description                    |");
+                                System.out.println("------------------------------------------------------");
+                                System.out.println("|   1    | Buy Stocks                                |");
+                                System.out.println("|   2    | Sell Stocks                               |");
+                                System.out.println("|   3    | Portfolio                                 |");
+                                System.out.println("|   4    | Create Trading Account                    |");
+                                System.out.println("|   5    | Profile                                   |");
+                                System.out.println("|   6    | Logout                                    |");
+                                System.out.println("------------------------------------------------------"+ANSI_RESET);
 
-                                case 2:
-                                    tradingController.sellStock();
-                                    break;
+                                System.out.print(ANSI_YELLOW+"Enter the Option to perform :"+ANSI_RESET);	
+                                int m = in.nextInt();
+                                switch(m)
+                                {
+                                    case 1:
+                                        tradingController.buyStock();
+                                        break;
 
-                                case 3:
-                                    tradingController.portfolio();
-                                    break;
+                                    case 2:
+                                        tradingController.sellStock();
+                                        break;
 
-                                case 4:
-                                    tradingController.createTradingAccount();
-                                    break;
+                                    case 3:
+                                        tradingController.portfolio();
+                                        break;
 
-                                case 5:
-                                    userController.profile();
-                                    break;
+                                    case 4:
+                                        tradingController.createTradingAccount();
+                                        break;
 
-                                case 6:
-                                    tradingDAO.closeConnection();
-                                    logout = userController.logout();
-                                    break;
-                                    
-                                default:
-                                    System.out.println("Enter the correct option to perform..");
-                                    break;
+                                    case 5:
+                                        userController.profile();
+                                        break;
+
+                                    case 6:
+                                        tradingDAO.closeConnection();
+                                        logout = userController.logout();
+                                        break;
+                                        
+                                    default:
+                                        System.out.println("Enter the correct option to perform..");
+                                        break;
+                                }
                             }
                         }
-                    }
-                    else
-                    {
-                        System.out.println("Login Failed");
-                    }
-                    break;
+                        else
+                        {
+                            System.out.println("Login Failed");
+                        }
+                        break;
 
-                case 2:
-                    userController.signUp();
-                    break;
+                    case 2:
+                        userController.signUp();
+                        break;
 
-                case 3:
-					System.out.println("Application Exiting");
-					System.exit(0);
-                    break;
-                
-                default:
-                    System.out.println("Enter the correct option to perform..");
-                    break;
+                    case 3:
+                        System.out.println("Application Exiting");
+                        System.exit(0);
+                        break;
+                    
+                    default:
+                        System.out.println("Enter the correct option to perform..");
+                        break;
+                }
             }
         }
+        catch(InputMismatchException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        
     }           
 }    
